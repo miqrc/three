2142a9bd1f02503034df0dd9aa919090cc690fd1
Ivan Danyliuk <ivan.daniluk@gmail.com>
2018-10-20 23:12:19 +0200

    Add properties to Scene

--- scenes_scene.go
+++ scenes_scene.go
@@ -9,9 +9,14 @@ import (
 // Scene - Allows you to set up what and where is to be rendered by three.js. This is where you place objects, lights and cameras.
 type Scene struct {
 	*js.Object
+
+	AutoUpdate bool   `js:"autoUpdate"`
+	Background *Color `js:"background"`
 }
 
 // NewScene - Create a new Scene object.
 func NewScene() *Scene {
-	return &Scene{Object: three.Get("Scene").New()}
+	return &Scene{
+		Object: three.Get("Scene").New(),
+	}
 }
